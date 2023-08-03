4e7e154411f4424c8a5e8947395a37da24e30481
akopton <kopton.olek@gmail.com>
2023-08-03 23:30:21 +0200

    feature: added sign out handling for AuthBtn

--- src/components/AuthBtn/AuthBtn.tsx
+++ src/components/AuthBtn/AuthBtn.tsx
@@ -1,16 +1,21 @@
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import styles from "./btn.module.css";
 
 type BtnProps = {
-  provider: string;
+  provider?: string;
+  signOut?: boolean;
 };
 
 export const AuthBtn = (props: BtnProps) => {
-  const handleClick = async () => await signIn(props.provider);
+  const handleClick = async () => {
+    !props.signOut ? await signIn(props.provider) : await signOut();
+  };
 
   return (
     <button type="button" onClick={handleClick} className={styles.btn}>
-      <span className={styles.btnText}>Sign In</span>
+      <span className={styles.btnText}>
+        {!props.signOut ? "Sign In" : "Sign Out"}
+      </span>
     </button>
   );
 };
