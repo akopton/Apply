e456b6a26397f86ff56f1ec1f0af9336992e56e0
akopton <kopton.olek@gmail.com>
2023-08-03 23:04:11 +0200

    feature: added Theme Context to handle dark and light themes

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useEffect, useState } from "react";
+
+type TContext = {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+const initialContext: TContext = {
+  theme: "light",
+  toggleTheme: () => console.log("theme"),
+};
+const ThemeContext = createContext(initialContext);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const toggleTheme = () => {
+    theme === "dark"
+      ? localStorage.setItem("theme", "light")
+      : localStorage.setItem("theme", "dark");
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme") as "light" | "dark";
+    if (theme) setTheme(theme);
+    else setTheme("light");
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={`${theme}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
