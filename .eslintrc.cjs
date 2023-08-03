c035749157f86ed15308fbe3ccc782b89067d3ee
akopton <kopton.olek@gmail.com>
2023-08-03 23:08:03 +0200

    styling: added styles for sign in button

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,11 +11,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
+    "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -24,6 +20,14 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
   },
 };
 
--- src/components/AuthBtn/AuthBtn.tsx
+++ src/components/AuthBtn/AuthBtn.tsx
@@ -1,15 +1,16 @@
 import { signIn } from "next-auth/react";
+import styles from "./btn.module.css";
 
 type BtnProps = {
   provider: string;
 };
 
 export const AuthBtn = (props: BtnProps) => {
-  const handleClick = () => signIn(props.provider);
+  const handleClick = async () => await signIn(props.provider);
 
   return (
-    <button type="button" onClick={handleClick}>
-      Sign In
+    <button type="button" onClick={handleClick} className={styles.btn}>
+      <span className={styles.btnText}>Sign In</span>
     </button>
   );
 };
