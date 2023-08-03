0c6abe015a4d1bd2a8ea3b5a3ba94f91cd6ba7dd
akopton <kopton.olek@gmail.com>
2023-08-03 23:29:30 +0200

    styling: added copy for home page and general layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import { AuthBtn } from "@/components/AuthBtn/AuthBtn";
+import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 
 export default function Home() {
+  const session = useSession();
+
   return (
     <>
       <Head>
-        <title>Apply</title>
+        <title>ApplyIT</title>
         <meta name="description" content="Job applications in one place" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-screen max-h-screen flex-col items-center justify-center gap-10 bg-primaryBg">
-        <span className="text-white">Hello there</span>
-        <span>Sign in with google</span>
-        <AuthBtn provider="google" />
+      <main className="flex h-screen max-h-screen flex-col items-center justify-center gap-10 bg-primaryBg p-10">
+        <h1 className="text-center text-4xl text-white">Welcome to ApplyIT!</h1>
+        <p className="text-center text-xl">
+          A perfect place to hold your job applications and track your progress!
+        </p>
+        <div className="flex flex-col items-center gap-3">
+          <span className="text-center text-xl">
+            If you want to start, sign in clicking button below
+          </span>
+          <AuthBtn provider="google" />
+        </div>
       </main>
     </>
   );
