0dd5715b57bf0931d2572fd9873bb485abbcac5f
akopton <kopton.olek@gmail.com>
2023-08-04 15:18:59 +0200

    feature: added Navbar to layout, layout styling

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,19 @@
 import { useSession } from "next-auth/react";
 import { ApplicationForm } from "../ApplicationForm/ApplicationForm";
-import styles from "./layout.module.css";
 import { useContext } from "react";
-import { CustomBtn } from "../CustomBtn/CustomBtn";
 import { ApplicationFormContext } from "@/context/FormContext";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "./layout.module.css";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { openForm, isFormOpened } = useContext(ApplicationFormContext);
-
+  const { isFormOpened } = useContext(ApplicationFormContext);
   const session = useSession();
 
   if (!session.data?.user) return children;
 
   return (
     <div className={styles.layout}>
-      <CustomBtn type="button" text="Open form" onClick={openForm} />
+      <Navbar />
       {children}
       <div
         className={styles.formWrapper}
@@ -24,7 +23,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
             : { height: "0", width: "0", transition: ".3s ease" }
         }
       >
-        <ApplicationForm />
+        {isFormOpened && <ApplicationForm />}
       </div>
     </div>
   );
