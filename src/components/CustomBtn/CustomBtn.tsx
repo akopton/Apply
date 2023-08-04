6955a214e33acce2fdc8c2010504c268585250b8
akopton <kopton.olek@gmail.com>
2023-08-04 15:19:35 +0200

    feature: added property to handle icons in CustomButton

--- src/components/CustomBtn/CustomBtn.tsx
+++ src/components/CustomBtn/CustomBtn.tsx
@@ -2,16 +2,18 @@ import styles from "./btn.module.css";
 
 type ButtonProps = {
   type: "button" | "submit" | "reset" | undefined;
-  text: string;
+  text?: string;
+  icon?: React.ReactNode;
   onClick: () => void;
 };
 
 export const CustomBtn = (props: ButtonProps) => {
-  const { type, text, onClick } = props;
+  const { type, text, icon, onClick } = props;
 
   return (
-    <button type={type} onClick={onClick}>
-      {text}
+    <button className={styles.btn} type={type} onClick={onClick}>
+      {text && text}
+      {icon && icon}
     </button>
   );
 };
