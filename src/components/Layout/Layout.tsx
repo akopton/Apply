946411e1067d218da9cc0b21ee824f2b0ff154c3
akopton <kopton.olek@gmail.com>
2023-08-04 12:10:01 +0200

    feature: added ApplicatiomForm component to layout as a modal

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,15 @@
 import { useSession } from "next-auth/react";
-
+import { ApplicationForm } from "../ApplicationForm/ApplicationForm";
+import styles from "./layout.module.css";
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const session = useSession();
 
   if (!session.data?.user) return children;
 
-  return <div>{children}</div>;
+  return (
+    <div className={styles.layout}>
+      {children}
+      <ApplicationForm />
+    </div>
+  );
 };
