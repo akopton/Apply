5fe6cee932c6c1ab0f3227b5fe6bef5ab51a3ed4
akopton <kopton.olek@gmail.com>
2023-08-06 15:31:12 +0200

    refactor: deleted name property from SearchPlatform model and changed Status name property to uniqe

---
+++ prisma/migrations/20230806132559_deleted/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `SearchPlatform` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "SearchPlatform" DROP COLUMN "name";
