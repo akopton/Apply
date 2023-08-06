70f8d5c1e604b7aaba10faeee30ff8c801ea0e9b
akopton <kopton.olek@gmail.com>
2023-08-06 15:10:50 +0200

    feature: Added new CustomTextarea component

---
+++ src/components/CustomTextarea/CustomTextarea.tsx
@@ -0,0 +1,20 @@
+import styles from "./textarea.module.css";
+
+type TextareaProps = {
+  value: string;
+  placeholder?: string;
+  onChange: (e: React.FormEvent<HTMLTextAreaElement>) => void;
+};
+
+export const CustomTextarea = (props: TextareaProps) => {
+  const { value, placeholder, onChange } = props;
+
+  return (
+    <textarea
+      className={styles.textField}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    ></textarea>
+  );
+};
