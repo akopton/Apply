0a472c90cf302692297c796e3c5c565e1c8ad872
akopton <kopton.olek@gmail.com>
2023-08-06 15:11:43 +0200

    feature: added input into the dropdown to allow users search the list

--- src/components/CustomDropdown/CustomDropdown.tsx
+++ src/components/CustomDropdown/CustomDropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import styles from "./dropdown.module.css";
 
 type DropdownProps = {
@@ -7,16 +7,24 @@ type DropdownProps = {
   selectedItem: string;
   expandIcon?: React.ReactElement;
   placeholder: string;
+  searchInput?: boolean;
 };
 
-const Dropdown = ({
-  data,
-  selectedItem,
-  onSelect,
-  expandIcon,
-  placeholder,
-}: DropdownProps) => {
+const Dropdown = (props: DropdownProps) => {
+  const { data, selectedItem, onSelect, expandIcon, placeholder, searchInput } =
+    props;
   const [showContent, setShowContent] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState("");
+  const filteredData = useMemo(() => {
+    if (searchValue === "") return data;
+    return data.filter((el) => el.includes(searchValue));
+  }, [searchValue, data]);
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const el = e.currentTarget.value;
+    setSearchValue(el);
+    onSelect(el);
+  };
 
   const Item = ({ el }: { el: string }) => {
     return (
@@ -43,24 +51,43 @@ const Dropdown = ({
     <div className={styles.dropdownWrapper}>
       <div
         className={styles.dropdown}
-        style={{ borderColor: showContent ? "var(--secondary-color)" : "" }}
+        style={showContent ? { borderColor: "var(--secondary-color)" } : {}}
       >
-        <button
-          type="button"
-          onClick={() => setShowContent((prevState) => !prevState)}
-          className={styles.dropdownSelectedItem}
-          style={{ justifyContent: expandIcon ? "space-between" : "center" }}
-        >
-          {selectedItem ? selectedItem : placeholder}
-          {expandIcon ? (
-            <div className={showContent ? "icon --opened" : "icon"}>
-              {expandIcon}
-            </div>
-          ) : null}
-        </button>
+        {!searchInput ? (
+          <button
+            type="button"
+            onClick={() => setShowContent((prevState) => !prevState)}
+            className={styles.dropdownSelectedItem}
+            style={{ justifyContent: expandIcon ? "space-between" : "center" }}
+          >
+            {selectedItem ? selectedItem : placeholder}
+            {expandIcon ? (
+              <div
+                className={
+                  showContent
+                    ? `${styles.icon as string} ${styles.opened as string}`
+                    : `${styles.icon as string}`
+                }
+              ></div>
+            ) : null}
+          </button>
+        ) : (
+          <div
+            className={styles.dropdownSelectedItem}
+            style={{ justifyContent: expandIcon ? "space-between" : "center" }}
+          >
+            <input
+              type="text"
+              onFocus={() => setShowContent(true)}
+              value={selectedItem ? selectedItem : searchValue}
+              onChange={handleChange}
+              placeholder={placeholder}
+            />
+          </div>
+        )}
         {showContent && (
           <ul className={styles.dropdownList}>
-            {data.map((el, id) => {
+            {filteredData.map((el, id) => {
               return typeof el === "object" ? (
                 <Item el={el.name} key={id} />
               ) : (
