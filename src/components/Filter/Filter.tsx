446e3220460f09af21e3abdb512d058954de551e
akopton <kopton.olek@gmail.com>
2023-08-08 14:25:56 +0200

    feature: added advanced filtering to applications list

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,16 +1,63 @@
 import { useState, useContext } from "react";
 import styles from "./filter.module.css";
 import { ApplicationsContext } from "@/context/ApplicationsContext";
+import { api } from "@/utils/api";
 
 export const Filter = () => {
   const [showFilters, setShowFilters] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const { search } = useContext(ApplicationsContext);
+  const platformList = api.searchPlatform.getAll.useQuery();
+  const statusList = api.status.getAll.useQuery();
+  const daysFiltersList = ["30", "14", "7"];
+
+  const {
+    searchByValue,
+    applyFilters,
+    filters: activeFilters,
+  } = useContext(ApplicationsContext);
 
   const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
     const value = e.currentTarget.value;
     setSearchValue(value);
-    search(value);
+    searchByValue(value);
+  };
+
+  const handleFilters = (type: string, value: string) => {
+    switch (type) {
+      case "days":
+        if (activeFilters?.days === "") {
+          applyFilters({ ...activeFilters, days: value });
+        } else {
+          if (activeFilters?.days !== value) {
+            applyFilters({ ...activeFilters, days: value });
+          } else {
+            applyFilters({ ...activeFilters, days: "" });
+          }
+        }
+        break;
+      case "status":
+        if (activeFilters?.statusId === "") {
+          applyFilters({ ...activeFilters, statusId: value });
+        } else {
+          if (activeFilters?.statusId !== value) {
+            applyFilters({ ...activeFilters, statusId: value });
+          } else {
+            applyFilters({ ...activeFilters, statusId: "" });
+          }
+        }
+        break;
+      case "platform":
+        if (activeFilters?.platformId === "") {
+          applyFilters({ ...activeFilters, platformId: value });
+        } else {
+          if (activeFilters?.platformId !== value) {
+            applyFilters({ ...activeFilters, platformId: value });
+          } else {
+            applyFilters({ ...activeFilters, platformId: "" });
+          }
+        }
+        break;
+    }
   };
 
   return (
@@ -35,7 +82,79 @@ export const Filter = () => {
         className={styles.filtersModal}
         style={{ bottom: showFilters ? "0" : "-100%" }}
       >
-        filters modal
+        <div className={styles.filtersSet}>
+          <span className={styles.filtersSetTitle}>Added X days ago:</span>
+          <div className={styles.filtersSetBtns}>
+            {daysFiltersList.map((el, idx) => {
+              return (
+                <button
+                  key={idx}
+                  type="button"
+                  className={styles.filterBtn}
+                  onClick={() => handleFilters("days", el)}
+                  style={{
+                    borderColor:
+                      activeFilters?.days === el
+                        ? "var(--secondary-color)"
+                        : "var(--primary-color)",
+                  }}
+                >
+                  {`> ${el}`}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.filtersSet}>
+          <span className={styles.filtersSetTitle}>Status:</span>
+          <div className={styles.filtersSetBtns}>
+            {statusList.data &&
+              statusList.data.map((el) => {
+                return (
+                  <button
+                    key={el.id}
+                    type="button"
+                    className={styles.filterBtn}
+                    onClick={() => handleFilters("status", el.id)}
+                    style={{
+                      borderColor:
+                        activeFilters?.statusId !== "" &&
+                        activeFilters?.statusId === el.id
+                          ? "var(--secondary-color)"
+                          : "var(--primary-color)",
+                    }}
+                  >
+                    {el.name}
+                  </button>
+                );
+              })}
+          </div>
+        </div>
+        <div className={styles.filtersSet}>
+          <span className={styles.filtersSetTitle}>Search platform:</span>
+          <div className={styles.filtersSetBtns}>
+            {platformList.data &&
+              platformList.data.map((el) => {
+                return (
+                  <button
+                    key={el.id}
+                    type="button"
+                    className={styles.filterBtn}
+                    onClick={() => handleFilters("platform", el.id)}
+                    style={{
+                      borderColor:
+                        activeFilters?.platformId !== "" &&
+                        activeFilters?.platformId === el.id
+                          ? "var(--secondary-color)"
+                          : "var(--primary-color)",
+                    }}
+                  >
+                    {el.url}
+                  </button>
+                );
+              })}
+          </div>
+        </div>
       </div>
       {showFilters && (
         <div className={styles.blur} onClick={() => setShowFilters(false)} />
--- src/context/ApplicationsContext.tsx
+++ src/context/ApplicationsContext.tsx
@@ -4,12 +4,34 @@ import { createContext, useMemo, useState } from "react";
 
 type TContext = {
   filteredData: Application[];
-  search: (value: string) => void;
+  searchByValue: (value: string) => void;
+  applyFilters: (filters: {
+    days?: string;
+    statusId?: string;
+    platformId?: string;
+  }) => void;
+  filters:
+    | {
+        days?: string;
+        statusId?: string;
+        platformId?: string;
+      }
+    | undefined;
 };
 
 const initialContext: TContext = {
   filteredData: [],
-  search: () => console.log("search"),
+  searchByValue: (value: string) => console.log("search"),
+  applyFilters: (filters: {
+    days?: string;
+    statusId?: string;
+    platformId?: string;
+  }) => console.log("filter"),
+  filters: {
+    days: "",
+    statusId: "",
+    platformId: "",
+  },
 };
 
 export const ApplicationsContext = createContext(initialContext);
@@ -20,27 +42,62 @@ export const ApplicationsProvider = ({
   children: React.ReactNode;
 }) => {
   const [searchValue, setSearchValue] = useState("");
+  const [filters, setFilters] = useState<{
+    days?: string;
+    statusId?: string;
+    platformId?: string;
+  }>();
   const { data } = api.application.getAll.useQuery();
 
   const filteredData = useMemo(() => {
     if (!data) return [];
-    if (searchValue) {
-      return data?.filter((el) => {
-        const position = el.position.toLowerCase();
-        const company = el.company.toLowerCase();
-        const value = searchValue.toLowerCase();
-        return position.includes(value) || company.includes(value);
-      });
+    if (searchValue || filters) {
+      return data
+        ?.filter((el) => {
+          const position = el.position.toLowerCase();
+          const company = el.company.toLowerCase();
+          const value = searchValue.toLowerCase();
+          return position.includes(value) || company.includes(value);
+        })
+        .filter((el) => {
+          const currentDate = new Date();
+          const addedAt = new Date(el.addedAt);
+          const days = parseInt(filters?.days as string);
+          const daysAgo = new Date(
+            currentDate.getTime() - days * 24 * 60 * 60 * 1000
+          );
+          return filters?.days && filters.days !== "" ? addedAt < daysAgo : el;
+        })
+        .filter((el) => {
+          return filters?.statusId && filters?.statusId !== ""
+            ? el.statusId === filters?.statusId
+            : el;
+        })
+        .filter((el) => {
+          return filters?.platformId && filters?.platformId !== ""
+            ? el.searchPlatformId === filters?.platformId
+            : el;
+        });
     }
     return data;
-  }, [searchValue, data]);
+  }, [searchValue, data, filters]);
 
-  const search = (value: string) => {
+  const searchByValue = (value: string) => {
     setSearchValue(value);
   };
 
+  const applyFilters = (filters: {
+    days?: string;
+    statusId?: string;
+    platformId?: string;
+  }) => {
+    setFilters(filters);
+  };
+
   return (
-    <ApplicationsContext.Provider value={{ filteredData, search }}>
+    <ApplicationsContext.Provider
+      value={{ filteredData, searchByValue, applyFilters, filters }}
+    >
       {children}
     </ApplicationsContext.Provider>
   );
