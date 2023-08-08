3b79babec6499e46b8cc84466723baf39df08689
akopton <kopton.olek@gmail.com>
2023-08-08 15:23:45 +0200

    styling: applications page styling

--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -13,11 +13,11 @@ export default function ApplicationsPage() {
         <title>Applications</title>
       </Head>
       <main className="relative h-full">
+        <div className="sticky top-0 flex w-full items-center justify-center bg-primaryBg p-2">
+          <Filter />
+        </div>
         <div className="h-[90%] w-full">
           <CustomList data={filteredData} />
-        </div>
-        <div className="sticky bottom-0 flex w-full items-center justify-center bg-primaryBg p-2">
-          <Filter />
         </div>
       </main>
     </>
