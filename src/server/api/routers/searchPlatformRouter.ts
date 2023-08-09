0d59d546889ad6684e1343f143489bf90bcd43f1
akopton <kopton.olek@gmail.com>
2023-08-10 00:42:07 +0200

    feature: added new route to searchPlatformRouter to get applications count

--- src/server/api/routers/searchPlatformRouter.ts
+++ src/server/api/routers/searchPlatformRouter.ts
@@ -1,8 +1,22 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const searchPlatformRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const platforms = await ctx.prisma.searchPlatform.findMany();
     return platforms;
   }),
+
+  getApplicationsForSinglePlatform: protectedProcedure.query(
+    async ({ ctx }) => {
+      const user = ctx.session.user;
+      const list = await ctx.prisma.searchPlatform.findMany({
+        include: {
+          _count: {
+            select: { applications: { where: { ownerId: user.id } } },
+          },
+        },
+      });
+      return list;
+    }
+  ),
 });
