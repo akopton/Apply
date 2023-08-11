e7a069288ccbd0cee4d797793c92598c495b5aa0
akopton <kopton.olek@gmail.com>
2023-08-11 11:24:29 +0200

    refactor: ProtectedRoute workaround for router returning promise

--- src/components/ProtectedRoute/ProtectedRoute.ts
+++ src/components/ProtectedRoute/ProtectedRoute.ts
@@ -1,29 +1,19 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter as useNavRouter } from "next/navigation";
+import { useRouter as useRouteRouter } from "next/router";
 import type { ReactNode } from "react";
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const session = useSession();
-  const router = useRouter();
+  const router = useNavRouter();
+  const { pathname } = useRouteRouter();
 
-  if (session.status === "unauthenticated" && router.pathname !== "/") {
-    async () => {
-      try {
-        await router.push("/");
-      } catch (e) {
-        console.error(e);
-      }
-    };
+  if (session.status === "unauthenticated" && pathname !== "/") {
+    router.push("/");
   }
 
-  if (session.status === "authenticated" && router.pathname === "/") {
-    async () => {
-      try {
-        await router.push("/");
-      } catch (e) {
-        console.error(e);
-      }
-    };
+  if (session.status === "authenticated" && pathname === "/") {
+    router.push("/dashboard");
   }
 
   return children;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,5 @@
-import { CustomBarChart } from "@/components/CustomBarChart/CustomBarChart";
 import { CustomList } from "@/components/CustomList/CustomList";
 import { api } from "@/utils/api";
-// import { useChartData } from "@/utils/useChartData";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -9,21 +7,6 @@ import Link from "next/link";
 export default function DashboardPage() {
   const { data: sessionData } = useSession();
   const applications = api.application.getTop.useQuery(4);
-
-  // const { data: applicationsCountForStatus } =
-  //   api.status.getApplicationsForSingleStatus.useQuery();
-
-  // const { data: applicationsCountForPlatform } =
-  //   api.searchPlatform.getApplicationsForSinglePlatform.useQuery();
-
-  // const { chartData: chartDataStatus, maxValue: maxValueStatus } = useChartData(
-  //   applicationsCountForStatus,
-  //   "name",
-  //   "_count.applications"
-  // );
-
-  // const { chartData: chartDataPlatforms, maxValue: maxValuePlatforms } =
-  //   useChartData(applicationsCountForPlatform, "url", "_count.applications");
 
   if (!sessionData?.user) {
     return <div>loading...</div>;
@@ -45,17 +28,6 @@ export default function DashboardPage() {
             Show all applications
           </Link>
         </div>
-        {/* <div className="flex flex-col items-center gap-4">
-          <span>Applications based on status:</span>
-          <CustomBarChart data={chartDataStatus} maxValue={maxValueStatus!} />
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <span>Applications based on searching platform:</span>
-          <CustomBarChart
-            data={chartDataPlatforms}
-            maxValue={maxValuePlatforms!}
-          />
-        </div> */}
       </main>
     </>
   );
