d434542fec7c718ee5c738aad7b17cb54b8b6b84
akopton <kopton.olek@gmail.com>
2023-08-30 15:14:48 +0200

    feature: added tablet/desktop view to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -48,47 +48,53 @@ export default function DashboardPage() {
       <Head>
         <title>Dashboard</title>
       </Head>
-      <main className="flex flex-col items-center gap-10 bg-primaryBg py-5">
-        <div className="flex w-full flex-col gap-3">
-          <h2 className="text-center text-2xl">Recently added applications</h2>
-          {applications.data && <ApplicationsList data={applications.data} />}
-          <Link
-            href={"/applications"}
-            className="self-center rounded-xl border-2 px-3 py-2"
-          >
-            Show all applications
-          </Link>
+      <main className="flex flex-col items-center gap-10 bg-primaryBg py-5 xl:text-xl">
+        <div className="grid grid-cols-1 max-md:gap-16 md:w-full md:grid-cols-2 md:px-10">
+          <div className="flex w-full flex-col gap-3 xl:px-40">
+            <h2 className="text-center text-2xl xl:text-3xl">
+              Recently added applications
+            </h2>
+            {applications.data && <ApplicationsList data={applications.data} />}
+            <Link
+              href={"/applications"}
+              className="self-center rounded-xl border-2 px-3 py-2"
+            >
+              Show all applications
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-3">
+            <h2 className="text-center text-2xl xl:text-3xl">
+              Currently you are applying for
+            </h2>
+            <span className="border-b-2 border-b-primaryColor px-2 text-center text-3xl">
+              <Link href={"/applications"}>{allApplicationsCount}</Link>
+            </span>
+            <span className="text-center text-2xl">offers.</span>
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-3">
-          <span className="text-center text-2xl">
-            Currently you are applying for
-          </span>
-          <span className="border-b-2 border-b-primaryColor px-2 text-center text-3xl">
-            <Link href={"/applications"}>{allApplicationsCount}</Link>
-          </span>
-          <span className="text-center text-2xl">offers.</span>
-        </div>
-        <div className="flex w-full flex-col items-center gap-2">
-          <span className="text-center text-2xl">
-            Your current applications based on status:
-          </span>
-          {applicationsForStatus && (
-            <FilledBarsList
-              data={mappedApplicationsForStatus}
-              maxValue={allApplicationsCount}
-            />
-          )}
-        </div>
-        <div className="flex w-full flex-col items-center gap-2">
-          <span className="text-center text-2xl">
-            Most popular search platforms:
-          </span>
-          {applicationsForPlatform && (
-            <FilledBarsList
-              data={mappedApplicationsForPlatform}
-              maxValue={allApplicationsCount}
-            />
-          )}
+        <div className="grid grid-cols-1 max-md:gap-16 md:w-full md:grid-cols-2 md:px-10">
+          <div className="flex w-full flex-col items-center gap-2">
+            <h2 className="text-center text-2xl xl:text-3xl">
+              Your current applications based on status:
+            </h2>
+            {applicationsForStatus && (
+              <FilledBarsList
+                data={mappedApplicationsForStatus}
+                maxValue={allApplicationsCount}
+              />
+            )}
+          </div>
+          <div className="flex w-full flex-col items-center gap-2">
+            <h2 className="text-center text-2xl xl:text-3xl">
+              Most popular search platforms:
+            </h2>
+            {applicationsForPlatform && (
+              <FilledBarsList
+                data={mappedApplicationsForPlatform}
+                maxValue={allApplicationsCount}
+              />
+            )}
+          </div>
         </div>
       </main>
     </>
